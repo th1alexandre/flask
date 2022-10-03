53216302486bbc7271d6c944d7b9bedb8bdce945
Th1Alexandre <th1alexandre.dev@gmail.com>
2022-10-03 19:08:28 -0400

    add: flasgger

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from database.db import db, SQLALCHEMY_DATABASE_URI
+from swagger import initialize_flasgger
 
 
 app = Flask(__name__)
@@ -8,6 +9,8 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
+
+swagger = initialize_flasgger(app)
 
 
 if __name__ == "__main__":
