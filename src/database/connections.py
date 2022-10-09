87ab26ffacc39eee9750419a3da174b140dbc1eb
Th1Alexandre <th1alexandre.dev@gmail.com>
2022-10-09 16:15:14 -0400

    add: default conn

--- src/database/connections.py
+++ src/database/connections.py
@@ -1,3 +1,4 @@
+import os
 import psycopg
 
 
@@ -30,3 +31,35 @@ def _connect_pg(
         )
     except Exception as e:
         raise Exception(f"Error connecting to PostgreSQL: {e}")
+
+
+def conn_default() -> psycopg.Connection:
+    """Connect to the default database, uses .env variables.
+
+    default:
+        user: os.getenv("POSTGRES_USER", "postgres")
+        password: os.getenv("POSTGRES_PASSWORD", "postgres")
+        host: os.getenv("POSTGRES_HOST", "postgres")
+        port: os.getenv("POSTGRES_PORT", 5432)
+        database: os.getenv("POSTGRES_DB", "flask")
+
+    Returns:
+        psycopg.Connection: A connection to the database.
+    """
+    try:
+        user = os.getenv("POSTGRES_USER", "postgres")
+        password = os.getenv("POSTGRES_PASSWORD", "postgres")
+        host = os.getenv("POSTGRES_HOST", "postgres")
+        port = os.getenv("POSTGRES_PORT", 5432)
+        database = os.getenv("POSTGRES_DB", "flask")
+
+        conn = _connect_pg(
+            user=user,
+            password=password,
+            host=host,
+            port=int(port),
+            database=database
+        )
+        return conn
+    except Exception as e:
+        raise Exception(f"Error connecting to PG - Default: {e}")
