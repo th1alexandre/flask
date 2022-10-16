68f5fdd8d251c8c9ba6af546590dce3b6405fdf4
Th1Alexandre <th1alexandre.dev@gmail.com>
2022-10-15 20:28:26 -0400

    add: flask secret key

--- src/main.py
+++ src/main.py
@@ -10,4 +10,10 @@ swagger = initialize_flasgger(app)
 
 if __name__ == "__main__":
     DEBUG = os.getenv("FLASK_DEBUG", "False") == "True"
+    SECRET_KEY = os.getenv("FLASK_SECRET_KEY").encode()
+
+    if not SECRET_KEY:
+        raise Exception("FLASK_SECRET_KEY not set")
+
+    app.secret_key = SECRET_KEY
     app.run(host="0.0.0.0", debug=DEBUG)
