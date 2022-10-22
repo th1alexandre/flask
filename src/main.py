d8f2033cedb085abbc74c8aac61619b8cba8af0e
Th1Alexandre <th1alexandre.dev@gmail.com>
2022-10-22 16:54:21 -0400

    refactor: change to app factory

--- src/main.py
+++ src/main.py
@@ -2,12 +2,21 @@ from config import FlaskConfig
 from flask import Flask
 from swagger import initialize_flasgger
 
-app = Flask(__name__)
 
-swagger = initialize_flasgger(app)
+def create_app():
+    try:
+        app = Flask(__name__)
+
+        initialize_flasgger(app)
+
+        config = FlaskConfig()
+        app.config.from_object(config)
+
+        return app
+    except Exception as e:
+        raise Exception(f"Error while creating app: {e}")
 
 
 if __name__ == "__main__":
-    config = FlaskConfig()
-    app.config.from_object(config)
+    app = create_app()
     app.run(host="0.0.0.0", port=5000)
