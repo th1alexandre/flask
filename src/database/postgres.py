6a743cd627314ad00e8111e89aa91dd3fbaadbf4
Th1Alexandre <th1alexandre.dev@gmail.com>
2022-10-29 01:56:06 -0400

    add: postgres conn

--- src/database/postgres.py
+++ src/database/postgres.py
@@ -1,3 +1,5 @@
+import os
+
 import psycopg
 
 
@@ -10,3 +12,18 @@ def _connect_pg(
         )
     except Exception as e:
         raise Exception(f"Error connecting to PostgreSQL: {e}")
+
+
+def conn_postgres():
+    try:
+        user = os.getenv("POSTGRES_USER", "postgres")
+        password = os.getenv("POSTGRES_PASSWORD", "postgres")
+        host = os.getenv("POSTGRES_HOST", "postgres")
+        port = os.getenv("POSTGRES_PORT", 5432)
+        database = os.getenv("POSTGRES_DB", "flask")
+
+        return _connect_pg(
+            user=user, password=password, host=host, port=int(port), database=database
+        )
+    except Exception as e:
+        raise Exception(f"Error while getting PG connection: {e}")
