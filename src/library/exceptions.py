f9d3503d1bf86cc028bf8c0e2959b6c505720055
Th1Alexandre <th1alexandre.dev@gmail.com>
2022-11-01 19:04:20 -0400

    add: base exception class

--- src/library/exceptions.py
+++ src/library/exceptions.py
@@ -1,38 +1,44 @@
-class BadRequestException(Exception):
-    def __init__(self, message):
+class BaseException(Exception):
+    def __init__(self, message, status_code):
+        self.status_code = status_code
         super().__init__(message)
 
 
-class UnauthorizedException(Exception):
+class BadRequestException(BaseException):
     def __init__(self, message):
-        super().__init__(message)
+        super().__init__(message, 400)
 
 
-class ForbiddenException(Exception):
+class UnauthorizedException(BaseException):
     def __init__(self, message):
-        super().__init__(message)
+        super().__init__(message, 401)
 
 
-class NotFoundException(Exception):
+class ForbiddenException(BaseException):
     def __init__(self, message):
-        super().__init__(message)
+        super().__init__(message, 403)
 
 
-class MethodNotAllowedException(Exception):
+class NotFoundException(BaseException):
     def __init__(self, message):
-        super().__init__(message)
+        super().__init__(message, 404)
 
 
-class ConflictException(Exception):
+class MethodNotAllowedException(BaseException):
     def __init__(self, message):
-        super().__init__(message)
+        super().__init__(message, 405)
 
 
-class PreconditionFailedException(Exception):
+class ConflictException(BaseException):
     def __init__(self, message):
-        super().__init__(message)
+        super().__init__(message, 409)
 
 
-class InternalServerErrorException(Exception):
+class PreconditionFailedException(BaseException):
     def __init__(self, message):
-        super().__init__(message)
+        super().__init__(message, 412)
+
+
+class InternalServerErrorException(BaseException):
+    def __init__(self, message):
+        super().__init__(message, 500)
