33509be127a3fec10786a6c7042133448e5861d8
Th1Alexandre <th1alexandre.dev@gmail.com>
2022-12-10 18:04:42 -0400

    docs: how to use this file

--- examples/sqlalchemy.py
+++ examples/sqlalchemy.py
@@ -1,3 +1,20 @@
+"""
+This file is a template for a SQLAlchemy table model.
+Copy the code below into a new file and change the examples to your needs.
+
+You have two options for storing your models: (if you have only one database and schema)
+    1. Create a new file and use the table name as the file name
+       Put this file inside src/database/models/"table_name".py
+       Each table in your database will be a new file inside models/
+    2. Create a new file called models.py or similar
+       Put this file inside src/database/models/
+       Each table in your database will be a new class inside models.py
+       Delete the src/database/models/ package and put the
+       "models.py" file inside src/database/
+
+I personally recommend option 2, as it is easier to use because you
+don't need to import multiple python modules (files) to use multiple models.
+"""
 from uuid import uuid4
 
 from sqlalchemy import Column, Integer, String
